044a02e807d9dd862978ed07e32488d659f6c2fb
Sibin Xavier <sibinx7@gmail.com>
2016-09-03 09:52:17 +0530

    Internship preference : student

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -18,6 +18,7 @@
                 <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
                   <div class="panel-body">
                     <form action="" method="post">
+                      {{csrf_field()}}
                       <div class="row">
                         <div class="col-xs-12 col-sm-6">
                           <div class="form-group"><label for="" class="control-label">First Name</label><input type="text"
@@ -122,13 +123,19 @@
                 <div class="panel-heading" role="tab" id="headingTwo">
                   <h4 class="panel-title">
                     <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                      Collapsible Group Item #2
+                      Internship Preferences
+                      <span class="glyphicon glyphicon-blackboard"></span>
                     </a>
                   </h4>
                 </div>
                 <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
                   <div class="panel-body">
-                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                    <form action="" method="POST">
+                      {{csrf_field()}}
+                      <div class="form-group text-right">
+                        <button class="btn" type="submit">Save</button>
+                      </div>
+                    </form>
                   </div>
                 </div>
               </div>
