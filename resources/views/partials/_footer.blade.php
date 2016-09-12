063a372e6729bb8fab9f2ddab3921026990f3559
Reshmarshenoy <reshmars007@gmail.com>
2016-09-12 14:29:20 +0530

    initial commit footer file

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,7 +1,61 @@
 <div class="footer-wrapper">
   <div class="container">
     <div class="row">
+     <div class="col-xs-4 ">
+       <h4>MOVIES BY GENRE</h4>
+       <ul>
+         <a href="#" title="Action"><span>Action</span></a><br>
+         <a href="#" title="Romance"><span>Romance</span></a><br>
+         <a href="#" title="Comedy"><span>Comedy</span></a><br>
+         <a href="#" title="Adult"><span>Adult</span></a><br>
+         <a href="#" title="Adventure"><span>Adventure</span></a><br>
+         <a href="#" title="Biography"><span>Biography</span></a><br>
+         <a href="#" title="Classic"><span>Classic</span></a><br>
+         <a href="#" title="Crime"><span>Crime</span></a><br>
+         <a href="#" title="Drama"><span>Drama</span></a><br>
+         <a href="#" title="Family"><span>Family</span></a>
+       </ul>
+     </div>
+      <div class="col-xs-4">
+        <h4>MOVIES BY GENRE</h4>
+        <ul>
+          <a href="#" title="Action"><span>Action</span></a><br>
+          <a href="#" title="Romance"><span>Romance</span></a><br>
+          <a href="#" title="Comedy"><span>Comedy</span></a><br>
+          <a href="#" title="Adult"><span>Adult</span></a><br>
+          <a href="#" title="Adventure"><span>Adventure</span></a><br>
+          <a href="#" title="Biography"><span>Biography</span></a><br>
+          <a href="#" title="Classic"><span>Classic</span></a><br>
+          <a href="#" title="Crime"><span>Crime</span></a><br>
+          <a href="#" title="Drama"><span>Drama</span></a><br>
+          <a href="#" title="Family"><span>Family</span></a>
+        </ul>
+      </div>
+      <div class="col-xs-4 ">
+        <h4>MOVIES BY GENRE</h4>
+        <ul>
+          <a href="#" title="Action"><span>Action</span></a><br>
+          <a href="#" title="Romance"><span>Romance</span></a><br>
+          <a href="#" title="Comedy"><span>Comedy</span></a><br>
+          <a href="#" title="Adult"><span>Adult</span></a><br>
+          <a href="#" title="Adventure"><span>Adventure</span></a><br>
+          <a href="#" title="Biography"><span>Biography</span></a><br>
+          <a href="#" title="Classic"><span>Classic</span></a><br>
+          <a href="#" title="Crime"><span>Crime</span></a><br>
+          <a href="#" title="Drama"><span>Drama</span></a><br>
+          <a href="#" title="Family"><span>Family</span></a>
+        </ul>
+      </div>
+    </div>
+    <div class="row">
+<hr>
+      </div>
+    <div class="row foot">
+        <h5><br>with  <span class="glyphicon glyphicon-heart red "></span> by RedPanthers</h5>
+      </div>
+
 
     </div>
+
   </div>
 </div>
