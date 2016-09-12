4eee4e5b061681c0467eb3f1337c4f74d56f3565
nimmyvipin <nimmyvipin25@gmail.com>
2016-09-12 12:56:14 +0530

    Admin login page added

--- resources/views/home/admin_login.blade.php
+++ resources/views/home/admin_login.blade.php
@@ -1,15 +1,22 @@
 @extends('layouts.home')
 @section('content')
+  <div class="loginpage">
   <div class="container">
-    <div class="row">
+
+      <div class="row">
       <div class="col-xs-12">
+        <div class="admin-form">
+
         <form action="{{route('login_admin')}}" method="POST">
           {{csrf_field()}}
           {{method_field('POST')}}
           <input type="email" name="email" required="" placeholder="Email">
           <input type="password" name="password" required="" placeholder="Password">
           <button type="submit">Login</button>
-        </form>
+         </form>
+          </div>
+      </div>
+
       </div>
     </div>
   </div>
