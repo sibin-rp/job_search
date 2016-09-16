6d63cd75c6ebeb86c51eb1c6c741ca7ed17901ad
Sibin Xavier <sibin@redpanthers.co>
2016-09-16 15:37:32 +0530

    Arrange footer list

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -1,61 +1,58 @@
 <div class="footer-wrapper">
   <div class="container">
     <div class="row">
-     <div class="col-xs-4 ">
-       <h4>MOVIES BY GENRE</h4>
-       <ul>
-         <a href="#" title="Action"><span>Action</span></a><br>
-         <a href="#" title="Romance"><span>Romance</span></a><br>
-         <a href="#" title="Comedy"><span>Comedy</span></a><br>
-         <a href="#" title="Adult"><span>Adult</span></a><br>
-         <a href="#" title="Adventure"><span>Adventure</span></a><br>
-         <a href="#" title="Biography"><span>Biography</span></a><br>
-         <a href="#" title="Classic"><span>Classic</span></a><br>
-         <a href="#" title="Crime"><span>Crime</span></a><br>
-         <a href="#" title="Drama"><span>Drama</span></a><br>
-         <a href="#" title="Family"><span>Family</span></a>
-       </ul>
-     </div>
+      <div class="col-xs-4 ">
+        <h4>MOVIES BY GENRE</h4>
+        <ul class="footer-list-ul">
+          <li><a href="#" title="Action"><span>Action</span></a></li>
+          <li><a href="#" title="Romance"><span>Romance</span></a></li>
+          <li><a href="#" title="Comedy"><span>Comedy</span></a></li>
+          <li><a href="#" title="Adult"><span>Adult</span></a></li>
+          <li><a href="#" title="Adventure"><span>Adventure</span></a></li>
+          <li><a href="#" title="Biography"><span>Biography</span></a></li>
+          <li><a href="#" title="Classic"><span>Classic</span></a></li>
+          <li><a href="#" title="Crime"><span>Crime</span></a></li>
+          <li><a href="#" title="Drama"><span>Drama</span></a></li>
+          <li><a href="#" title="Family"><span>Family</span></a></li>
+        </ul>
+      </div>
       <div class="col-xs-4">
         <h4>MOVIES BY GENRE</h4>
-        <ul>
-          <a href="#" title="Action"><span>Action</span></a><br>
-          <a href="#" title="Romance"><span>Romance</span></a><br>
-          <a href="#" title="Comedy"><span>Comedy</span></a><br>
-          <a href="#" title="Adult"><span>Adult</span></a><br>
-          <a href="#" title="Adventure"><span>Adventure</span></a><br>
-          <a href="#" title="Biography"><span>Biography</span></a><br>
-          <a href="#" title="Classic"><span>Classic</span></a><br>
-          <a href="#" title="Crime"><span>Crime</span></a><br>
-          <a href="#" title="Drama"><span>Drama</span></a><br>
-          <a href="#" title="Family"><span>Family</span></a>
+        <ul class="footer-list-ul">
+          <li><a href="#" title="Action"><span>Action</span></a></li>
+          <li><a href="#" title="Romance"><span>Romance</span></a></li>
+          <li><a href="#" title="Comedy"><span>Comedy</span></a></li>
+          <li><a href="#" title="Adult"><span>Adult</span></a></li>
+          <li><a href="#" title="Adventure"><span>Adventure</span></a></li>
+          <li><a href="#" title="Biography"><span>Biography</span></a></li>
+          <li><a href="#" title="Classic"><span>Classic</span></a></li>
+          <li><a href="#" title="Crime"><span>Crime</span></a></li>
+          <li><a href="#" title="Drama"><span>Drama</span></a></li>
+          <li><a href="#" title="Family"><span>Family</span></a></li>
         </ul>
       </div>
       <div class="col-xs-4 ">
         <h4>MOVIES BY GENRE</h4>
-        <ul>
-          <a href="#" title="Action"><span>Action</span></a><br>
-          <a href="#" title="Romance"><span>Romance</span></a><br>
-          <a href="#" title="Comedy"><span>Comedy</span></a><br>
-          <a href="#" title="Adult"><span>Adult</span></a><br>
-          <a href="#" title="Adventure"><span>Adventure</span></a><br>
-          <a href="#" title="Biography"><span>Biography</span></a><br>
-          <a href="#" title="Classic"><span>Classic</span></a><br>
-          <a href="#" title="Crime"><span>Crime</span></a><br>
-          <a href="#" title="Drama"><span>Drama</span></a><br>
-          <a href="#" title="Family"><span>Family</span></a>
+        <ul class="footer-list-ul">
+          <li><a href="#" title="Action"><span>Action</span></a></li>
+          <li><a href="#" title="Romance"><span>Romance</span></a></li>
+          <li><a href="#" title="Comedy"><span>Comedy</span></a></li>
+          <li><a href="#" title="Adult"><span>Adult</span></a></li>
+          <li><a href="#" title="Adventure"><span>Adventure</span></a></li>
+          <li><a href="#" title="Biography"><span>Biography</span></a></li>
+          <li><a href="#" title="Classic"><span>Classic</span></a></li>
+          <li><a href="#" title="Crime"><span>Crime</span></a></li>
+          <li><a href="#" title="Drama"><span>Drama</span></a></li>
+          <li><a href="#" title="Family"><span>Family</span></a></li>
         </ul>
       </div>
     </div>
-    <div class="row">
-<hr>
-      </div>
     <div class="row foot">
-        <h5><br>with  <span class="glyphicon glyphicon-heart red "></span> by RedPanthers</h5>
-      </div>
-
-
+      <h5>with <span class="glyphicon glyphicon-heart red "></span> by RedPanthers</h5>
     </div>
 
+
   </div>
+
+</div>
 </div>
